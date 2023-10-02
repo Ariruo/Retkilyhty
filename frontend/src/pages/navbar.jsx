6286445852fbcd894b2bb5e9e65986594f59cf97
Ari <ari.ruotsalainenn@gmail.com>
2023-10-03 00:30:12 +0300

    styling

--- frontend/src/pages/navbar.jsx
+++ frontend/src/pages/navbar.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -32,35 +30,18 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-orange-900  py-4 font-sans rounded-b-lg">
+    <nav className="bg-orange-900 py-2 font-sans">
       <div className="container mx-auto px-4 flex items-center justify-center">
         {/* Logo */}
-        <button onClick={handleHomeClick} className="text-white font-semibold text-xl focus:outline-none absolute left-1/2 transform -translate-x-1/2">
+        <button onClick={handleHomeClick} className="text-white  center font-semibold text-xl focus:outline-none">
           TrailTorch
         </button>
 
-        {/* Dropdown */}
-        <div className="relative ml-auto" ref={dropdownRef}>
-          <button className="text-white hover:text-gray-600 transition duration-300 ease-in-out focus:outline-none" onClick={toggleDropdown}>
-            <FontAwesomeIcon icon={faBars} size="lg" />
-          </button>
-          {isDropdownOpen && (
-            <div className="absolute top-12 right-0 bg-black py-2 px-4 rounded-lg navbar-dropdown z-10">
-              <ul className="space-y-2">
-                <li>
-                  <Link to="/" onClick={toggleDropdown} className="text-white hover:text-gray-600 transition duration-300 ease-in-out">
-                    Koti
-                  </Link>
-                </li>
-                <li>
-                  <a href="#" className="text-white hover:text-gray-600 transition duration-300 ease-in-out">
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </div>
-          )}
-        </div>
+      
+       
+          
+          
+        
       </div>
     </nav>
   );
